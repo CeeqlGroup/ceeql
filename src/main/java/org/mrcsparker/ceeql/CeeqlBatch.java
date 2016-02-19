ec0c314cc8626cc9c57940030352502c340e8fc1
Rogelio Mendoza <Rogelio_Mendoza-Avellaneda@mckinsey.com>
2016-02-19 14:22:33 -0500

    Refactor CeeqlTemplate.apply to propagate handlebars exceptions; This make the error handling logic more robust, instead of just sending back an empty string which was creating side effects at runtime when using invalid query object. No query binding or query execution is perform at the sql level until handlebars syntax is fixed.

--- src/main/java/org/mrcsparker/ceeql/CeeqlBatch.java
+++ src/main/java/org/mrcsparker/ceeql/CeeqlBatch.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.skife.jdbi.v2.Batch;
 import org.skife.jdbi.v2.Handle;
 
+import java.io.IOException;
 import java.util.Map;
 
 class CeeqlBatch {
@@ -21,7 +22,7 @@ class CeeqlBatch {
         this.args = args;
     }
 
-    public String exec() {
+    public String exec() throws IOException {
         Batch batch = dbiHandle.createBatch();
         batch.add(CeeqlTemplate.apply(sql, args));
 
--- src/main/java/org/mrcsparker/ceeql/CeeqlInsert.java
+++ src/main/java/org/mrcsparker/ceeql/CeeqlInsert.java
@@ -1,10 +1,12 @@
 package org.mrcsparker.ceeql;
 
 import org.skife.jdbi.v2.Handle;
+
+import java.io.IOException;
 import java.util.Map;
 
 class CeeqlInsert extends CeeqlAction {
-    public CeeqlInsert(Handle dbiHandle, String sql, Map<String, String> args) {
+    public CeeqlInsert(Handle dbiHandle, String sql, Map<String, String> args) throws IOException {
         super(dbiHandle, sql, args);
     }
 }
