4d3be0273cfe808e0660d6a734669282d7d8a41d
Eric Cadwell <e_cadwell@hotmail.com>
2017-12-01 13:36:47 -0700

    additional helpers, regex replace all

--- src/test/java/org/mrcsparker/ceeql/handlebars/FilterHelperTest.java
+++ src/test/java/org/mrcsparker/ceeql/handlebars/FilterHelperTest.java
@@ -9,140 +9,235 @@ import org.junit.Test;
 public class FilterHelperTest extends AbstractHelperTest {
 
 	@Test
-    public void safeRegistersTest() throws IOException {
+	public void safeRegistersTest() throws IOException {
 
-    	assertEquals("", 
-        		apply("{{safe this}}",
-        			null
-        			)
-        	);
+		assertEquals("",
+				apply("{{safe this}}",
+					null
+					)
+			);
 
 	}
 
-    @Test
-    public void canInsertIntoTest() throws IOException {
-        assertEquals("name", 
-        		apply("{{safe .}}",
-        			"name"
-        			)
-        	);
-    }
+	@Test
+	public void canInsertIntoTest() throws IOException {
+		assertEquals("name",
+				apply("{{safe .}}",
+					"name"
+					)
+			);
+	}
 
-    @Test
-    public void canHandleNumbers() throws IOException {
-        assertEquals("1", 
-        		apply("{{safe .}}",
-        			"1"
-        			)
-        	);
-    }
+	@Test
+	public void canHandleNumbers() throws IOException {
+		assertEquals("1",
+				apply("{{safe .}}",
+					"1"
+					)
+			);
+	}
 
-    @Test
-    public void handlesQuotesTest() throws IOException {
-        assertEquals("'name'", 
-        		apply("{{safe .}}",
-        			"'name'"
-        			)
-        	);
-    }
+	@Test
+	public void handlesQuotesTest() throws IOException {
+		assertEquals("'name'",
+				apply("{{safe .}}",
+					"'name'"
+					)
+			);
+	}
 
-    @Test
-    public void handlesSQLInjectionTest() throws IOException {
-        assertEquals("dataset1; drop table dataset2", 
-        		apply("{{safe .}}",
-        			"dataset1; drop table dataset2"
-        			)
-        	);
-    }
+	@Test
+	public void handlesSQLInjectionTest() throws IOException {
+		assertEquals("dataset1; drop table dataset2",
+				apply("{{safe .}}",
+					"dataset1; drop table dataset2"
+					)
+			);
+	}
 
-    @Test
-    public void identifierTest() throws IOException {
-    	
-    	// Sql injection
-    	try {
-    		apply("{{identifier .}}",
-    			"dataset1; drop table dataset2"
-        	);
-        	fail();
-    	} catch(Exception e) {
-    		assertEquals("Input not a valid identifier.", e.getCause().getMessage());
-    	}
+	@Test
+	public void identifierTest() throws IOException {
 
-        assertEquals("one,two,three", 
-        		apply("{{identifier .}}",
-        			new String[] {"one", "two", "three"}
-        			)
-        	);
+		// Sql injection
+		try {
+			apply("{{identifier .}}",
+				"dataset1; drop table dataset2"
+			);
+			fail();
+		} catch(Exception e) {
+			assertEquals("Input not a valid identifier.", e.getCause().getMessage());
+		}
 
-        assertEquals("one,two,three", 
-        		apply("{{identifier .}}",
-        			"one,two,three"
-        			)
-        	);
+		assertEquals("one,two,three",
+				apply("{{identifier .}}",
+					new String[] {"one", "two", "three"}
+					)
+			);
 
-        assertEquals("a.b.c, a.b.d, a.b.e", 
-        		apply("{{identifier .}}",
-        			"a.b.c, a.b.d, a.b.e"
-        			)
-        	);
+		assertEquals("one,two,three",
+				apply("{{identifier .}}",
+					"one,two,three"
+					)
+			);
 
-        assertEquals("\"select\"", 
-        		apply("{{identifier .}}",
-        			"\"select\""
-        			)
-        	);
+		assertEquals("a.b.c, a.b.d, a.b.e",
+				apply("{{identifier .}}",
+					"a.b.c, a.b.d, a.b.e"
+					)
+			);
 
-        assertEquals("one", 
-        		apply("{{#identifier}}{{this}}{{/identifier}}",
-        			"one"
-        			)
-        	);
+		assertEquals("\"select\"",
+				apply("{{identifier .}}",
+					"\"select\""
+					)
+			);
 
-        assertEquals("one", 
-        		apply("{{concat (identifier .)}}",
-        			"one"
-        			)
-        	);
+		assertEquals("one",
+				apply("{{#identifier}}{{this}}{{/identifier}}",
+					"one"
+					)
+			);
 
-    }
+		assertEquals("one",
+				apply("{{concat (identifier .)}}",
+					"one"
+					)
+			);
 
-    @Test
-    public void numberTest() throws IOException {
-    	
-    	// Sql injection
-    	try {
-    		apply("{{number .}}",
-    			"dataset1; drop table dataset2"
-        	);
-        	fail();
-    	} catch(Exception e) {
-    		assertEquals("Input not a valid number.", e.getCause().getMessage());
-    	}
+	}
 
-        assertEquals("1,2,3", 
-        		apply("{{number .}}",
-        			new Integer[] {1, 2, 3}
-        			)
-        	);
+	@Test
+	public void numberTest() throws IOException {
 
-        assertEquals("1,2,3", 
-        		apply("{{number .}}",
-        			"1,2,3"
-        			)
-        	);
+		// Sql injection
+		try {
+			apply("{{number .}}",
+				"dataset1; drop table dataset2"
+			);
+			fail();
+		} catch(Exception e) {
+			assertEquals("Input not a valid number.", e.getCause().getMessage());
+		}
 
-        assertEquals("1,2,3", 
-        		apply("{{#number}}{{this}}{{/number}}",
-        			"1,2,3"
-        			)
-        	);
+		// Signed number
+		try {
+			apply("{{number .}}",
+				"-1"
+			);
+			fail();
+		} catch(Exception e) {
+			assertEquals("Input not a valid number.", e.getCause().getMessage());
+		}
 
-        assertEquals("1", 
-        		apply("{{concat (number .)}}",
-        			"1"
-        			)
-        	);
-        
-    }
+		// Decimal number
+		try {
+			apply("{{number .}}",
+				"1.0"
+			);
+			fail();
+		} catch(Exception e) {
+			assertEquals("Input not a valid number.", e.getCause().getMessage());
+		}
+
+		assertEquals("1,2,3",
+				apply("{{number .}}",
+					new Integer[] {1, 2, 3}
+					)
+			);
+
+		assertEquals("1,2,3",
+				apply("{{number .}}",
+					"1,2,3"
+					)
+			);
+
+		assertEquals("1,2,3",
+				apply("{{#number}}{{this}}{{/number}}",
+					"1,2,3"
+					)
+			);
+
+		assertEquals("1",
+				apply("{{concat (number .)}}",
+					"1"
+					)
+			);
+
+		assertEquals("773",
+				apply("{{number .}}",
+					"773"
+					)
+			);
+	}
+
+	@Test
+	public void decimalTest() throws IOException {
+
+		// Sql injection
+		try {
+			apply("{{decimal .}}",
+				"dataset1; drop table dataset2"
+			);
+			fail();
+		} catch(Exception e) {
+			assertEquals("Input not a valid number.", e.getCause().getMessage());
+		}
+
+		assertEquals("1",
+				apply("{{concat (decimal .)}}",
+					"1"
+					)
+			);
+
+		assertEquals("-10",
+				apply("{{decimal .}}",
+					"-10"
+					)
+			);
+
+		assertEquals("773",
+				apply("{{decimal .}}",
+					"773"
+					)
+			);
+
+		assertEquals(".76",
+				apply("{{decimal .}}",
+					".76"
+					)
+			);
+
+		assertEquals("0.76",
+				apply("{{decimal .}}",
+					"0.76"
+					)
+			);
+
+		assertEquals("-760.0",
+				apply("{{decimal .}}",
+					"-760.0"
+					)
+			);
+
+		assertEquals("773.44",
+				apply("{{decimal .}}",
+					"773.44"
+					)
+			);
+	}
+
+	@Test
+	public void sqlTest() throws IOException {
+
+				System.out.println(apply("{{sql .}}",
+					"10; select pg_sleep(10); --"
+					));
+
+		assertEquals("10 select pg_sleep(10)",
+				apply("{{sql .}}",
+					"10; select pg_sleep(10); --"
+					)
+			);
+	}
 
 }
